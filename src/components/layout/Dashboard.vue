c844a6c6128a280ce3ce94a66be4ae80542f811a
muasya-victor <vicmwe184@gmail.com>
2024-02-07 16:55:44 +0300

    mobile menu init

--- src/components/layout/Dashboard.vue
+++ src/components/layout/Dashboard.vue
@@ -10,6 +10,11 @@ const formData = ref({
   }
 })
 
+const menuDialog = ref(false)
+const toggleMenuDialog = ()=>{
+  menuDialog.value = !menuDialog.value
+}
+
 
 const fleet = ref([
   {
@@ -39,11 +44,26 @@ const fleet = ref([
         </div>
 
       </el-aside>
+
       <el-main style="padding: 7px">
         <div class="bg-gray-100 w-full h-full rounded p-4">
 
-          <div class="w-full p-1 flex justify-end items-center ">
-            <span class="cursor-pointer">
+          <el-dialog v-model="menuDialog" title="Menu" class="sm:w-[300px] md:w-[800px]">
+            <div class="w-full flex flex-col items-center h-full">
+              <el-avatar class=""
+                         src="src/assets/images/car.png"
+                         fit="cover"
+                         style="background-color: white; color: black"
+                         :size="100"/>
+
+              <p class="font-semibold text-hbr-primary">Fleet Manager</p>
+
+              <RouterLinks/>
+            </div>
+          </el-dialog>
+
+          <div class="w-full p-1 sm:flex justify-end items-center md:hidden">
+            <span class="cursor-pointer" @click="toggleMenuDialog">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
               </svg>
