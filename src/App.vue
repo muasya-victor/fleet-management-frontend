09351a49c2bb903f2c8c378cbf74f450bdc87baa
muasya-victor <vicmwe184@gmail.com>
2024-03-22 23:48:10 +0300

    connect to backend

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import Dashboard from "./components/layout/Dashboard.vue";
-import FleetHealth from "@/pages/fleet/FleetHealth.vue";
 </script>
 
 <template>
   <div class="">
-    <Dashboard>
-      <router-view/>
-    </Dashboard>
+
+    <router-view/>
+
   </div>
 </template>
 
